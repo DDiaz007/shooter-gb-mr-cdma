42da28bee49b4eeb05d15e35990abfab456b8ff9
ddiaz <ddiaz591@gmail.com>
2012-06-10 15:43:35 -0700

    initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Jun 9 00:16:38 PDT 2012"
+#define LINUX_COMPILE_TIME "00:16:38"
+#define LINUX_COMPILE_BY "ddiaz"
+#define LINUX_COMPILE_HOST "ddiaz-desktop"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
