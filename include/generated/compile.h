f9510fa3265e434e2f0699c718cc28db542dc7a5
ddiaz <ddiaz591@gmail.com>
2012-06-10 22:55:52 -0700

    disable ext2 FS/ set tun as M/ enable cifs/ enable NLS

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Sat Jun 9 00:16:38 PDT 2012"
-#define LINUX_COMPILE_TIME "00:16:38"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Jun 10 20:06:33 PDT 2012"
+#define LINUX_COMPILE_TIME "20:06:33"
 #define LINUX_COMPILE_BY "ddiaz"
 #define LINUX_COMPILE_HOST "ddiaz-desktop"
 #define LINUX_COMPILE_DOMAIN "(none)"
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "2.6.35.13"
+#define UTS_RELEASE "2.6.35.14-PURE-TEST3-g34f31fd-dirty"
